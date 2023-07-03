7226137542691b33248bf77bbdecf3a1f2becae1
Lengocphuong05 <132881217+Lengocphuong05@users.noreply.github.com>
2023-07-03 21:28:21 +0700

    Create pdo_category.php

---
+++ pdo_category.php
@@ -0,0 +1,50 @@
+<?php
+$DB_TYPE = 'mysql';
+$DB_HOST = 'localhost';
+$DB_NAME = 'lesson8';
+$USER = 'root';
+$PW = '';
+
+try {
+    $connection = new PDO("$DB_TYPE:host=$DB_HOST;dbname=$DB_NAME", $USER, $PW);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+    function prepareSQL($sql){
+        global $conn;
+        return $conn->prepare($sql);
+    }
+    // Lấy dữ liệu ra nè
+    function getData(){
+        $sql = "SELECT * FROM category";
+        $select = prepareSQL($sql);
+        $select->execute();
+        return $select->fetchAll();
+    }
+
+    function getOneData($data){
+        $sql = "SELECT * FROM category WHERE id = :id";
+        $select = prepareSQL($sql);
+        $select->execute($data);
+        return $select->fetchAll();
+    }
+
+    function createNewData($data){
+        $sql = "INSERT INTO category (name) VALUES (:name)";
+        $create = prepareSQL($sql);
+        $create->execute($data);
+    }
+
+    function updateData($data){
+        $sql = "UPDATE category SET name = :name WHERE id = :id";
+        $update = prepareSQL($sql);
+        $update->execute($data);
+    }
+    function deleteData($data){
+        $sql = "DELETE FROM category WHERE id = :id";
+        $update = prepareSQL($sql);
+        $update->execute($data);
+    }
+?>
